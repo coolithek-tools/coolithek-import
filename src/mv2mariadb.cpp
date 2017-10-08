fde70790bdf47264bdaac9dbaef3daff55306f55
M. Liebmann <git@tuxcode.de>
2017-10-08 19:54:09 +0200

    Set version to 0.4.0

--- src/mv2mariadb.cpp
+++ src/mv2mariadb.cpp
@@ -20,7 +20,7 @@
 	Boston, MA  02110-1301, USA.
 */
 
-#define PROGVERSION "0.3.10"
+#define PROGVERSION "0.4.0"
 #define DBVERSION "3.0"
 #define PROGNAME "mv2mariadb"
 #define DEFAULTXZ "mv-movielist.xz"
